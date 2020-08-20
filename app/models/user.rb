ab62574542639b7eeb5eab7361ec21388aaf99f5
elee <ericlee.2311@gmail.com>
2020-08-20 17:02:08 -0400

    finish backend user model

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+class User < ApplicationRecord
+    #Basic validations for user
+    validates :email, presense: true, uniqueness: true
+    validates :password, length: { 6, allow_nil: true}
+    validates :session_token, presense: true, uniquness: true
+
+    #Ensure that a user is logged in if a new user instance is created
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    #Generating a new session token
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    #Resetting session token
+    def self.reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token 
+    end
+
+    #Ensuring a session token
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    #Password creation
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    #Checking for valid password
+    def is_password?(password)
+        encrypted = BCrypt::Password.new(self.password_digest)
+        encrypted.is_password?(password)
+    end
+
+    #Checking for existing user
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        #Returning nil if a user does not exist
+        return nil unless user && user.is_password?(password)
+
+        #Return user if user found
+        user
+    end
+
+end
---
+++ frontend/eventswipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    //Component to render to 'root' element
+    ReactDOM.render(<h1>Eventswipe v1.5</h1>, root);
+})
