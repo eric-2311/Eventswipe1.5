7be5e2832b16ff3605c5e59be378adaa2dcd3cfb
elee <ericlee.2311@gmail.com>
2020-08-21 17:48:57 -0400

    add login logout and getUsers to session api util

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -6,3 +6,28 @@ export const signup = user => {
         data: { user }
     })
 }
+
+//AJAX promise which posts to session for logging in
+export const login = user => {
+    return $.ajax({
+        method: 'POST',
+        url: '/api/session',
+        data: { user }
+    })
+}
+
+//AJAX promise which deletes to session for logging out
+export const logout = () => {
+    return $.ajax({
+        method: 'DELETE',
+        url: '/api/session'
+    })
+}
+
+//AJAX promise which gets to users to fetch all users
+export const getUsers = () => {
+    return $.ajax({
+        method: 'GET',
+        url: '/api/users'
+    })
+}
