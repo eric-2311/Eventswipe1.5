d822725287a661c3d0bbe78dafce5ab137c7ff45
elee <ericlee.2311@gmail.com>
2020-08-24 16:55:24 -0400

    altered App.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 
 const App = () => (
-    <div>Welcome to Eventswipe v1.5</div>
+    <div>Welcome to Eventswipe v1.5!</div>
 )
 
 export default App;
