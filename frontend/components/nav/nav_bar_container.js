898a543e5cc423978bc840b75dade86d82a0e1e5
elee <ericlee.2311@gmail.com>
2020-08-28 20:14:58 -0400

    fixed mDTP in nav bar container

--- frontend/components/nav/nav_bar_container.js
+++ frontend/components/nav/nav_bar_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import NavBar from './Nav_bar';
 import { logout } from '../../actions/session_actions';
 
-//Map State to Props to allow for current user to accessed through NavBar component
+//Map State to Props to allow for current user to be accessed through NavBar component
 const mSTP = ({ session, entities: { users } }) => ({
     currentUser: users[session.id]
 })
@@ -12,4 +12,5 @@ const mDTP = dispatch => ({
     logout: () => dispatch(logout())
 })
 
+//Using React-Redux connect to connect mSTP and mDTP to NavBar component
 export default connect(mSTP, mDTP)(NavBar);
