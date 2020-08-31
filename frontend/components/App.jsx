f255eb60ab608a0090c71e9d244398b80c6e30db
elee <ericlee.2311@gmail.com>
2020-08-31 17:03:34 -0400

    add splash page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import NavBarContainer from './nav/nav_bar_container';
+import Splash from './splash/Splash';
 
 const App = () => (
     <div>
         <NavBarContainer />
+        <Splash />
     </div>
 )
 
---
+++ frontend/components/splash/Splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Splash extends React.Component {
+    render() {
+        return (
+            <div>This is the splash component</div>
+        )
+        
+    }
+}
+
+export default Splash;
