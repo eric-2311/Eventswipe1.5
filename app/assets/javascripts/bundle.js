e8c76d1e6101e9fc683d8db9f3c061b9196dd4b1
elee <ericlee.2311@gmail.com>
2020-09-02 18:14:32 -0400

    add link tags to logo

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -232,6 +232,7 @@ var App = function App() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -256,6 +257,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 
 
+
 var NavBar = /*#__PURE__*/function (_React$Component) {
   _inherits(NavBar, _React$Component);
 
@@ -275,13 +277,15 @@ var NavBar = /*#__PURE__*/function (_React$Component) {
       if (this.props.currentUser) {
         return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
           className: "nav-bar-container"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/",
           className: "nav-bar-logo"
         }, "eventswipe")); //If a user is not logged in, render the button linking to the login/signup page
       } else {
         return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
           className: "nav-bar-container"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+          to: "/",
           className: "nav-bar-logo"
         }, "eventswipe"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
           className: "nav-bar-sign-in"
--- frontend/components/nav/Nav_bar.jsx
+++ frontend/components/nav/Nav_bar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
     //Passing props through constructor
@@ -11,14 +12,14 @@ class NavBar extends React.Component {
         if (this.props.currentUser) {
             return (
                 <div className="nav-bar-container">
-                    <h1 className="nav-bar-logo">eventswipe</h1>
+                    <Link to="/" className="nav-bar-logo">eventswipe</Link>
                 </div>
             )
         //If a user is not logged in, render the button linking to the login/signup page
         } else {
             return (
                 <div className="nav-bar-container">
-                    <h1 className="nav-bar-logo">eventswipe</h1>
+                    <Link to="/" className="nav-bar-logo">eventswipe</Link>
                     <h1 className="nav-bar-sign-in">Sign In</h1>
                 </div>
             )
