7f41c4e2fef5ddc74973c97ddc990f9c04ce1653
elee <ericlee.2311@gmail.com>
2020-09-10 17:43:10 -0400

    add mSTP to login form container

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -1,2 +1,7 @@
 import { connect } from 'react-redux';
 import LoginForm from './login_form';
+
+//Map State to Props to allow access to data in login form
+const mSTP = state => ({
+    email: state.session.email
+})
