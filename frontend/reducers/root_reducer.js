16d8a8c0eccd4359964a63c4588239ba63520587
elee <ericlee.2311@gmail.com>
2020-09-12 19:52:04 -0400

    add errors to slice of state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 //Importing necessary reducers to construct slices of state
 import sessionReducer from './session_reducer';
 import entitiesReducer from './entities_reducer';
+import errorsReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
     session: sessionReducer, 
-    entities: entitiesReducer
+    entities: entitiesReducer,
+    errors: errorsReducer
 })
 
 export default rootReducer;
