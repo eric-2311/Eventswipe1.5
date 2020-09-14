ceedb7cd684381710140f3d2e6e94a3ce12b82c2
elee <ericlee.2311@gmail.com>
2020-09-14 15:40:27 -0400

    add Map Dispatch to Props to login form container

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import LoginForm from './login_form';
+import { login, clearErrors, validateEmail } from '../../actions/session_actions';
 
 //Map State to Props to allow access to data in login form
 const mSTP = state => ({
@@ -8,5 +9,9 @@ const mSTP = state => ({
 })
 
 const mDTP = dispatch => ({
-    
+    processForm: user => dispatch(login(user)),
+    receiveEmail: email => dispatch(validateEmail(email)),
+    removeErrors: () => dispatch(clearErrors())
 })
+
+export default connect(mSTP, mDTP)(LoginForm);
