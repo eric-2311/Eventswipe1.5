3dfa693c3aa6fb6efbd526b8562f826ef0ddffda
elee <ericlee.2311@gmail.com>
2020-09-20 16:42:40 -0400

    add basic layout for login form

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+}
